972700e82751c53792737c13f84e1272739d32bf
GY Lee <93250312+ThinkGYLee@users.noreply.github.com>
2026-06-27 23:51:24 +0900

    [Passed] 정사각형 두 개 출력 (101ms, 9MB)

---
+++ trail1/정사각형 두 개 출력/output-two-rectangles.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val n = readln().toInt()
+    for(i in 0..n-1) {
+        for(j in 0..n-1) {
+            print("*")
+        }
+        println()
+    }
+    println()
+    for(i in 0..n-1) {
+        for(j in 0..n-1) {
+            print("*")
+        }
+        println()
+    }
+}
